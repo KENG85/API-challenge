d024478e8125af62efed3707d4c4f4ba57067614
Kate Engard <kateengard@Kates-MacBook-Pro.local>
2020-04-11 18:48:09 -0700

    removed API keys

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "f667b794c167e30624db44e0f77f703e"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyAtsbtthexPCL1-OK5xQOEN77O1aIGu7qY"
+g_key = ""
